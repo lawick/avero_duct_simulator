f0b0b0f0f5f9ba0de7d00590167f170742842ce0
Simon Laptop <simarnold@ethz.ch>
2024-04-11 13:54:25 +0200

    modified:   rotors_gazebo_plugins/include/rotors_gazebo_plugins/motor_model_servo.hpp

--- rotors_gazebo_plugins/include/rotors_gazebo_plugins/motor_model_servo.hpp
+++ rotors_gazebo_plugins/include/rotors_gazebo_plugins/motor_model_servo.hpp
@@ -165,12 +165,12 @@ class MotorModelServo : public MotorModel {
     switch (mode_) {
       case (ControlMode::kPosition): {
         if (!std::isnan(ref_motor_rot_pos_)) {
-          // joint_controller_->SetPositionTarget(joint_->GetScopedName(),
-          //                                      turning_direction_ * ref_motor_rot_pos_);
+          joint_controller_->SetPositionTarget(joint_->GetScopedName(),
+                                               turning_direction_ * ref_motor_rot_pos_);
           //joint_controller_->SetJointPosition(joint_ ->GetScopedName(), turning_direction_ * ref_motor_rot_pos_);
-          joint_->SetPosition(0, turning_direction_ * ref_motor_rot_pos_);
+          //joint_->SetPosition(0, turning_direction_ * ref_motor_rot_pos_);
 
-          //joint_controller_->Update();
+          joint_controller_->Update();
         }
         break;
       }
